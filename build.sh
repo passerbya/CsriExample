2fbc0c6f6dcfc6735da98c7765f9159293f62ba5
wurui <1341531859@qq.com>
2018-07-25 12:10:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-tcc -Isrc -lass src/libass_csri.c  src/posix/openfile.c -run src/test.c data/test.ass
+# tcc -Isrc -lass src/libass_csri.c  src/posix/openfile.c -run src/test.c data/test.ass
+gcc -Isrc -lass src/libass_csri.c  src/posix/openfile.c src/test.c -o test && ./test data/test.ass
