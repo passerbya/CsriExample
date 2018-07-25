fc1731032a9aafddd061f9293d5477d995f42d45
wurui1994 <1341531859@qq.com>
2018-07-25 14:17:02 +0800

    add libass_csri dll.

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
-REM tcc -I. -L. -lass libass_csri.c  win32/openfile.c -run test.c test.ass
-REM cl /I. /c libass_csri.c win32/openfile.c test.c
-set PATH=bin;%PATH%;
 mkdir out
+set PATH=bin;%PATH%;
+REM cl /nologo /c /w /EHsc /Isrc src\*.c src\win32\*.c /Fo.\out\
+REM link /nologo /dll /def:lib\libass_csri.def out\*.obj lib\libass.lib
+REM tcc -Isrc -Lbin -lass_csri -run src\test.c data\test.ass
+cl /Isrc lib/libass_csri.lib test.c
 if exist test.bmp del test.bmp
-cl /nologo  /w /EHsc /Isrc src\*.c src\win32\*.c /Fo.\out\ /Fe:test.exe lib\libass.lib
 test.exe data\test.ass
 del test.exp test.lib
 rd /s/q out
