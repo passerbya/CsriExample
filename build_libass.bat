a7cc9c37592935bb2a644e72b2f269a9757dbc5c
wangzh <wangzhenghang@1sj.tv>
2022-04-07 15:39:03 +0800

    FEAT：增加pdb文件编译，增加LineSpacing行间距功能

--- build_libass.bat
+++ build_libass.bat
@@ -2,11 +2,11 @@
 set PATH=bin;%PATH%;
 if exist test.bmp del test.bmp
 mkdir out_libass
-cl /nologo  /c /w /EHsc /Isrc /Isrc\libass /Isrc\win32 src\win32\openfile.c src\libass\*.c /Fo:.\out_libass\
-link /nologo /dll /def:lib\libass_csri.def out_libass\*.obj /out:libass_csri.dll lib\libass.lib
+cl /DEBUG /Zi /nologo  /c /w /EHsc /Isrc /Isrc\libass /Isrc\win32 src\win32\openfile.c src\libass\*.c /Fo:.\out_libass\
+link /DEBUG /nologo /dll /def:lib\libass_csri.def out_libass\*.obj /out:libass_csri.dll /PDB:"csri\libass_csri.pdb" lib\libass.lib
 move libass_csri.dll csri
 move libass_csri.lib lib
-del *.exp
+del *.exp *.ilk
 tcc -Isrc -Lbin -lcsri -run test\test.c data\test.ass
 REM tcc -I. -Lbin -lVSFilter -run src\test.c data\test.ass
 REM rd /s/q out_libass
